15212d7d6094d44e4351836139cb9bd23b56af91
AJ Seidler <ajseidler0526@gmail.com>
2023-12-11 13:48:17 -0600

    added outlet to route from page to page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,26 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import HomePage from './pages/HomePage.jsx';
+import ErrorPage from './pages/ErrorPage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />
+      },      
+    ]
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
+
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+ 
+  return (
+    <>
+      <h3>homepage</h3>
+    </>
+  );
+}
