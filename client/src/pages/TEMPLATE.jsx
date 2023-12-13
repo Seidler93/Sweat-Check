143ec5af230148c9bef7f1d2254e42780572548a
AJ Seidler <ajseidler0526@gmail.com>
2023-12-13 13:21:35 -0600

    added template page

---
+++ client/src/pages/TEMPLATE.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect  } from 'react';
+import { Link } from 'react-router-dom';
+import Header from '../components/Header/index'
+import HomeMenu from '../components/HomeMenu';
+
+export default function TEMPLATE() {
+  const [showMenu, setShowMenu] = useState(false);
+  return (
+    <>
+      <Header showMenu={showMenu} setShowMenu={setShowMenu} />
+      {showMenu ? (
+        <HomeMenu />
+        ) : (
+        <>
+          {/* put code for the page here */}
+        </>
+      )}
+    </>
+  );
+};
