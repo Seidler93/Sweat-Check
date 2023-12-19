9af7eef2a200ee20556faa59e11afde36ab1389b
AJ Seidler <ajseidler0526@gmail.com>
2023-12-18 21:45:28 -0600

    updated homepage layout

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -15,19 +15,19 @@ export default function Header ({ showMenu, setShowMenu}) {
   };
 
   return (
-    <Nav className='bg-dark d-flex justify-content-between fixed-top'>
-      <NavDropdown  title="Sweat Check" id="nav-dropdown" className='ps-2 d-flex align-items-center justify-content-center'>
+    <Nav className='f2 bg-dark d-flex justify-content-between fixed-top'>
+      <NavDropdown  title="Sweat Check" id="nav-dropdown" className='ps-3 d-flex align-items-center justify-content-center'>
         <NavDropdown.Item >Feed 1</NavDropdown.Item>
         <NavDropdown.Item >Feed 2</NavDropdown.Item>
         <NavDropdown.Item >Feed 3</NavDropdown.Item>
       </NavDropdown>
-      <div className='d-flex'>
-        <Nav.Item className='p-1'>
+      <div className='d-flex pe-3'>
+        <Nav.Item className='p-2 px-3'>
           <Link to={'/conversations'}>
             <FontAwesomeIcon icon={faMessage} style={{color: "#ffffff",}} className='message-icon'/>
           </Link>
         </Nav.Item>
-        <Nav.Item className='p-1'>
+        <Nav.Item className='p-2'>
           <Nav.Link >            
             <FontAwesomeIcon
               icon={showMenu ? faXmark : faBars}
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -45,11 +45,16 @@ export default function HomePage() {
           <button className='see-all-btn'><Link to={'/store'}>See All</Link></button>
 
             <h2 className='ms-3'>Friends</h2> 
-            <div className='d-flex flex-column'>
+            <div className='d-flex flex-column friends-container'>
               <button className='friend-btn'><Link to={'/store/friendId'}>First Last</Link><p>Status</p></button>
               <button className='friend-btn'><Link to={'/store/friendId'}>First Last</Link><p>Status</p></button>
               <button className='friend-btn'><Link to={'/store/friendId'}>First Last</Link><p>Status</p></button>
               <button className='friend-btn'><Link to={'/store/friendId'}>First Last</Link><p>Status</p></button>
+              <button className='friend-btn'><Link to={'/store/friendId'}>First Last</Link><p>Status</p></button>              
+              <button className='friend-btn'><Link to={'/store/friendId'}>First Last</Link><p>Status</p></button>              
+              <button className='friend-btn'><Link to={'/store/friendId'}>First Last</Link><p>Status</p></button>              
+              <button className='friend-btn'><Link to={'/store/friendId'}>First Last</Link><p>Status</p></button>              
+              <button className='friend-btn'><Link to={'/store/friendId'}>First Last</Link><p>Status</p></button>              
               <button className='friend-btn'><Link to={'/store/friendId'}>First Last</Link><p>Status</p></button>              
             </div> 
           <Link to={'/store/programId'}>buy program</Link>
