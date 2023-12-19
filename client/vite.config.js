e1b10bfffe56a3e2d281fa73cfaf8b27aac73bec
AJ Seidler <ajseidler0526@gmail.com>
2023-12-19 15:10:28 -0600

    added ability to download as pwa

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,52 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+
+const manifestForPlugin = {
+  registerType: 'prompt',
+  includeAssets: ['placeholderPortrait.jpg'],
+  manifest: {
+    name: 'Sweat Check',
+    short_name: 'SC',
+    description: 'An app to find and track workouts and progress',
+    icons: [
+      {
+        "src": "manifest-icon-192.maskable.png",
+        "sizes": "192x192",
+        "type": "image/png",
+        "purpose": "any"
+      },
+      {
+        "src": "manifest-icon-192.maskable.png",
+        "sizes": "192x192",
+        "type": "image/png",
+        "purpose": "maskable"
+      },
+      {
+        "src": "manifest-icon-512.maskable.png",
+        "sizes": "512x512",
+        "type": "image/png",
+        "purpose": "any"
+      },
+      {
+        "src": "manifest-icon-512.maskable.png",
+        "sizes": "512x512",
+        "type": "image/png",
+        "purpose": "maskable"
+      }
+    ],
+    theme_color: '#171717',
+    background_color: '#e8ebf2',
+    display: 'fullscreen',
+    scope: '/',
+    start_url: '/',
+    orientation: 'portrait',
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugin)],
   server: {
     port: 3000,
     open: true,
@@ -14,5 +57,8 @@ export default defineConfig({
         secure: false,
       },
     },
+  },
+  build: {
+    chunkSizeWarningLimit: 1500, // Adjust as needed
   },
 });
