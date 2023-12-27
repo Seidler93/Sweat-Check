7b1266458dc50a0860c2cc4ba9041a78100761a2
AJ Seidler <ajseidler0526@gmail.com>
2023-12-27 05:20:28 -0600

    removed extra code in calendar

--- client/src/pages/CalendarPage.jsx
+++ client/src/pages/CalendarPage.jsx
@@ -9,25 +9,12 @@ import { useUserContext } from "../utils/UserContext";
 export default function CalendarPage(){
   const [showMenu, setShowMenu] = useState(false);
   const [date,setDate] = useState(new Date());
-  // const onChange = (selectedDate) => {
-  //     setDate(selectedDate);
-  //   };
+  
   let datesWorkedOut = [ '2023-12-25','2023-12-24','2023-12-23',]
   const dateCheckedIn = JSON.parse(localStorage.getItem('checkedIn')) || '';
 
   datesWorkedOut = [...datesWorkedOut, dateCheckedIn]
   const {checkedIn, setCheckedIn} = useUserContext()
-
-  // Function to determine the background color for a specific date
-  const getTileContent = ({ date, view }) => {
-    if (view === 'month') {
-      const dateString = date.toISOString().split('T')[0];
-      if (datesWorkedOut.includes(dateString)) {
-        return <div style={{ background: 'green', padding: '2px' }} />;
-      }
-    }
-    return null;
-  };
 
   // Function to handle date change
   const onChange = (newDate) => {
