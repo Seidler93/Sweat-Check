4ab8bdccf54b58de01fb6f84aec72c04368640d5
AJ Seidler <ajseidler0526@gmail.com>
2023-12-27 05:14:10 -0600

    worked on calendar functionality

--- client/src/pages/CalendarPage.jsx
+++ client/src/pages/CalendarPage.jsx
@@ -12,7 +12,7 @@ export default function CalendarPage(){
   // const onChange = (selectedDate) => {
   //     setDate(selectedDate);
   //   };
-  let datesWorkedOut = [ '2023-12-24','2023-12-25','2023-12-23',]
+  let datesWorkedOut = [ '2023-12-25','2023-12-24','2023-12-23',]
   const dateCheckedIn = JSON.parse(localStorage.getItem('checkedIn')) || '';
 
   datesWorkedOut = [...datesWorkedOut, dateCheckedIn]
@@ -41,10 +41,74 @@ export default function CalendarPage(){
   };
 
   useEffect(() => {
-    console.log('clicked');
     const dateCheckedIn = JSON.parse(localStorage.getItem('checkedIn')) || '';
     datesWorkedOut = [...datesWorkedOut, dateCheckedIn]
   }, [checkedIn])
+
+  function countConsecutiveDates(dateArray) {
+    if (dateArray.length === 0) {
+      return 0; // Empty array has no consecutive dates
+    }
+    console.log(dateArray);
+    let count = 1;
+    let currentDate = new Date(dateArray[0]);
+  
+    for (let i = 1; i < dateArray.length; i++) {
+      const nextDate = new Date(dateArray[i]);
+      const oneDay = 24 * 60 * 60 * 1000; // Number of milliseconds in a day
+  
+      if (nextDate - currentDate === oneDay) {
+        count++;
+        currentDate = nextDate;
+      } else {
+        break; // Break the sequence if the next date is not one day ahead
+      }
+    }
+    console.log(count);
+    return count;
+  }
+
+  let longestStreak = 0;
+  let currentStreak = 0;
+
+  const calculateWorkoutStreak = (dates) => {
+    if (dates.length === 0) {
+      return 0; // No workouts, so streak is 0
+    }
+  
+    // Sort the dates in descending order (newest to oldest)
+    const sortedDates = dates.sort((a, b) => new Date(b) - new Date(a));
+    // console.log(sortedDates);
+  
+    let currentStreak = 0;
+  
+    
+    // Check if the current date is consecutive with yesterday's date
+    const lastCheckedIn = (sortedDates[0]);
+    const yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+    const year = yesterday.getFullYear();
+    const month = yesterday.getMonth() + 1; // Months are zero-indexed, so add 1
+    const day = yesterday.getDate();
+    
+    // Format the date as a string
+    const formattedDate = `${year}-${month < 10 ? '0' : ''}${month}-${day < 10 ? '0' : ''}${day}`;
+
+    // console.log('last checked in date:',lastCheckedIn);
+    // console.log('yesterday:',formattedDate);
+
+    if (lastCheckedIn === formattedDate) {
+      // Consecutive days
+      currentStreak = countConsecutiveDates(sortedDates);
+    }
+  
+    return currentStreak;
+  };
+
+
+  const streak = calculateWorkoutStreak(datesWorkedOut);
+
+
 
     return (
         <>
@@ -57,6 +121,8 @@ export default function CalendarPage(){
               {/* <Calendar onChange={onChange} value = {date} className={'text-black'}/> */}
               {/* <Calendar onChange={onChange} value = {date}/> */}
               <Calendar onChange={onChange} value={date} tileClassName={getTileClassName} />
+              <h3 className='text-white'>Longest Sreak:{longestStreak}</h3>
+              <h3 className='text-white'>Current Sreak:{currentStreak}</h3>
           </div>
         </>
       )}
