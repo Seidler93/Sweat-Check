85562bb19292d0bdb012b833ff29b403cf2fe447
AJ Seidler <ajseidler0526@gmail.com>
2023-12-29 13:48:53 -0600

    updated profile page css

--- client/src/components/profile/ProfileHeader.jsx
+++ client/src/components/profile/ProfileHeader.jsx
@@ -2,7 +2,7 @@ import PlaceholderPortrait from '../../assets/placeholderPortrait.jpg'
 
 export default function ProfileHeader () {
   return (
-    <div className='profile-container'>
+    <div className='profile-container hp text-white'>
       <div>
         <h1>User Name</h1>
         <p>Bio</p>
