ea34931a7d41f90c2e2b2666a384fae41003ddd8
AJ Seidler <ajseidler0526@gmail.com>
2024-01-05 10:49:41 -0600

    updated css

--- client/src/components/profile/MyPosts.jsx
+++ client/src/components/profile/MyPosts.jsx
@@ -3,7 +3,7 @@ import MyPostCard from "./MyPostCard";
 export default function MyPosts () {
   const posts = [1,1,1,1,1,1,1,1,1,11,]
   return (
-    <div className='card, bg-light post'>
+    <div className='card bg-light post'>
       {posts.map((post) => <MyPostCard post={post}/>)}
     </div>
   );
