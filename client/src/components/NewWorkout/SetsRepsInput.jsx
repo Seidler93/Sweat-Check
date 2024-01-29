560ff795e8310f20cfbbd8b972d2a36319af07a0
AJ Seidler <ajseidler0526@gmail.com>
2024-01-29 07:56:33 -0600

    added functionality where you click on input field for reps or weight and it will automatically highlight the text field so that you dont have to hightlight it yourself to change the input

--- client/src/components/NewWorkout/SetsRepsInput.jsx
+++ client/src/components/NewWorkout/SetsRepsInput.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect  } from 'react';
+import { useState, useEffect, useRef  } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSquareCheck } from '@fortawesome/free-solid-svg-icons';
 
@@ -6,6 +6,8 @@ export default function SetsRepsInput({setIndex, handleSetComplete, completedSet
   const [repsInput, setRepsInput] = useState(setInfo.reps || 0);
   const [weightInput, setWeightInput] = useState(setInfo.weight || 0);
   const [completedSet, setCompletedSet] = useState(setInfo.completed || false)
+  const repsInputRef = useRef(null);
+  const weightInputRef = useRef(null);
 
   useEffect(() => {
     if (completedSet) {
@@ -22,10 +24,22 @@ export default function SetsRepsInput({setIndex, handleSetComplete, completedSet
     setRepsInput(value);
     handleSetChange(setIndex, value, weightInput, completedSet);
   };
+
+  const handleRepsInputClick = () => {
+    if (repsInputRef.current) {
+      repsInputRef.current.select();
+    }
+  };
   
   const handleWeightChange = (value) => {
     setWeightInput(value);
     handleSetChange(setIndex, repsInput, value, completedSet);
+  };
+
+  const handleWeightInputClick = () => {
+    if (weightInputRef.current) {
+      weightInputRef.current.select();
+    }
   };
   
   const handleCompleteChange = () => {
@@ -39,18 +53,22 @@ export default function SetsRepsInput({setIndex, handleSetComplete, completedSet
       <div key={setIndex} className='d-flex align-items-center justify-content-start mb-2'>
         <p className='text-white p-2 m-0 w20 text-center'>{setIndex + 1}</p>
         <input 
-          className='w30 px-2 me-1 form-control' 
+          className='w30 px-2 me-1 form-control text-black' 
           type="number" 
           placeholder='reps' 
           value={repsInput}
           onChange={(e) => handleRepsChange(parseInt(e.target.value, 10))}
+          onFocus={handleRepsInputClick}
+          ref={repsInputRef}
         />
         <input 
-          className='w30 px-2 ms-1 form-control' 
+          className='w30 px-2 ms-1 form-control text-black' 
           type="number" 
           placeholder='weight' 
           value={weightInput}
           onChange={(e) => handleWeightChange(parseInt(e.target.value, 10))}
+          onFocus={handleWeightInputClick}
+          ref={weightInputRef}
         />
         <button
           onClick={() => handleCompleteChange()}
